0e3668af4c53a32ae08d7327d33f10671546e553
章旭楠 <zhang_xn1314@163.com>
2018-02-22 15:54:48 +0800

    eureka server api

--- plat-cloud/web-demo/src/main/java/com/ngdsoft/web/WebDemoApplication.java
+++ plat-cloud/web-demo/src/main/java/com/ngdsoft/web/WebDemoApplication.java
@@ -1,14 +1,21 @@
 package com.ngdsoft.web;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
-@EnableFeignClients
+@EnableFeignClients(basePackages = "com.ngdsoft.client.**")//单独 jar 包需要指定 package
 @SpringBootApplication
 public class WebDemoApplication {
+
+    @Bean
+    Logger.Level feginLoggerLevel() {
+        return Logger.Level.FULL;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(WebDemoApplication.class, args);
