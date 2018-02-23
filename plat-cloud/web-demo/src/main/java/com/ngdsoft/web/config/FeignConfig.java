0b85760690c5ca9740cd6be2d9519f4674a275eb
章旭楠 <zhang_xn1314@163.com>
2018-02-23 11:10:04 +0800

    使用 okhttp

--- plat-cloud/web-demo/src/main/java/com/ngdsoft/web/config/FeignConfig.java
+++ plat-cloud/web-demo/src/main/java/com/ngdsoft/web/config/FeignConfig.java
@@ -1,11 +1,20 @@
 package com.ngdsoft.web.config;
 
 import com.ngdsoft.client.user.UserServiceClient;
+import feign.Feign;
 import feign.Logger;
+import okhttp3.ConnectionPool;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.cloud.netflix.feign.FeignAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.concurrent.TimeUnit;
+
 @Configuration
+@ConditionalOnClass(Feign.class)
+@AutoConfigureBefore(FeignAutoConfiguration.class)
 public class FeignConfig {
     @Bean
     Logger.Level feginLoggerLevel() {
@@ -15,5 +24,16 @@ public class FeignConfig {
     //@Bean
     public UserServiceClient.DefaultFallback userServiceClientFallback() {
         return new UserServiceClient.DefaultFallback();
+    }
+
+    @Bean
+    public okhttp3.OkHttpClient okHttpClient() {
+        return new okhttp3.OkHttpClient.Builder()
+                .readTimeout(60, TimeUnit.SECONDS)
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .writeTimeout(120, TimeUnit.SECONDS)
+                .connectionPool(new ConnectionPool())
+                // .addInterceptor();
+                .build();
     }
 }
