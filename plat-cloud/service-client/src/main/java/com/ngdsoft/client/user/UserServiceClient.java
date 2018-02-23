f7399180082e19cddd01ba95b4a76406906a4147
章旭楠 <zhang_xn1314@163.com>
2018-02-23 08:49:32 +0800

    eureka server api

--- plat-cloud/service-client/src/main/java/com/ngdsoft/client/user/UserServiceClient.java
+++ plat-cloud/service-client/src/main/java/com/ngdsoft/client/user/UserServiceClient.java
@@ -3,8 +3,18 @@ package com.ngdsoft.client.user;
 
 import com.ngdsoft.api.user.UserService;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.stereotype.Component;
 
 
-@FeignClient(value = "user-center", path = "/user-api")
+@FeignClient(value = "user-center", path = "/user-api"/*, fallback = UserServiceClient.DefaultFallback.class*/)
 public interface UserServiceClient extends UserService {
+    /**
+     * 容错处理类，当调用失败时，简单返回空字符串
+     */
+    @Component
+    class DefaultFallback implements UserServiceClient {
+        public String hello(String name) {
+            return "服务不可用";
+        }
+    }
 }
